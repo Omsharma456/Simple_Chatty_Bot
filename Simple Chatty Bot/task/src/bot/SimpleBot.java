b389ca44bf62d37c9eec50089466b18f9acaf197
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-03 19:11:36 +0530

    It is simply count method

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -25,4 +25,12 @@ public class SimpleBot {
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
+
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+    }
 }
